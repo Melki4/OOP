39bc7c0db2601762ed7e2b9cc2820d6cfe671275
Nikolay PC <karikh.nickolay@icloud.com>
2025-10-05 00:11:54 +0400

    Added point class

---
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/functions/Point.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk._repfor2lab_._OOP_.functions;
+
+public class Point {
+
+    public final double x;
+    public final double y;
+
+    Point(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+}
