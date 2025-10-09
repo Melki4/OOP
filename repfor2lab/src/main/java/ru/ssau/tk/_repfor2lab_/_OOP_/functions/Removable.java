cc8c526534f4c4d4bb9b617da7a206783c648788
Nickolay <karikh.nickolay@icloud.com>
2025-10-10 00:23:42 +0400

    Delete repfor2lab/src directory

--- repfor2lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/functions/Removable.java
+++
@@ -1,5 +0,0 @@
-package ru.ssau.tk._repfor2lab_._OOP_.functions;
-
-public interface Removable {
-    void remove(int index);
-}
--- repfor2lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/functions/ZeroFunction.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk._repfor2lab_._OOP_.functions;
-
-public class ZeroFunction extends ConstantFunction{
-    public ZeroFunction(){
-        super(0);
-    }
-}
