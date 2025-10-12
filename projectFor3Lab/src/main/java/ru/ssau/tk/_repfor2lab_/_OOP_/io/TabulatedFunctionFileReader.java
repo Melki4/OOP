1ed05536c35dc355524e2389b76672a3221161b3
Nikolay PC <karikh.nickolay@icloud.com>
2025-10-12 22:28:15 +0400

    deleted extra imports

--- projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/io/TabulatedFunctionFileReader.java
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/io/TabulatedFunctionFileReader.java
@@ -1,12 +1,9 @@
 package ru.ssau.tk._repfor2lab_._OOP_.io;
 
-import ru.ssau.tk._repfor2lab_._OOP_.functions.ArrayTabulatedFunction;
 import ru.ssau.tk._repfor2lab_._OOP_.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk._repfor2lab_._OOP_.functions.factory.LinkedListTabulatedFunctionFactory;
-import ru.ssau.tk._repfor2lab_._OOP_.functions.factory.TabulatedFunctionFactory;
 
 import java.io.*;
-import java.text.ParseException;
 
 public class TabulatedFunctionFileReader {
     public static void main(String[] args){
