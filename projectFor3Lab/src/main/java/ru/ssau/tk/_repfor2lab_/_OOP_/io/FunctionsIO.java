4d8c1a98ee285d771a1b0204c3bd0d14171068ab
s4vl2 <lera0682@mail.ru>
2025-10-12 23:55:51 +0400

    добавлен метод deserialize в FunctionsIO

--- projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/io/FunctionsIO.java
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/io/FunctionsIO.java
@@ -64,4 +64,22 @@ final class FunctionsIO {
         boof.writeObject(function);
         boof.flush();
     }
+    static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        int count = dataInputStream.readInt();
+
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+
+        for (int i = 0; i < count; i++){
+            xValues[i] = dataInputStream.readDouble();
+            yValues[i] = dataInputStream.readDouble();
+        }
+        return factory.create(xValues, yValues);
+    }
+    static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
+        ObjectInputStream stream1 = new ObjectInputStream(stream);
+        return (TabulatedFunction) stream1.readObject();
+    }
+
 }
