c3b10411aaebbfbcda697c07a7a95de6d5504e0f
Nikolay PC <karikh.nickolay@icloud.com>
2025-10-12 18:34:39 +0400

    added FunctionsIO class and done tests 4 him

--- projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/io/FunctionsIO.java
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/io/FunctionsIO.java
@@ -1,7 +1,26 @@
 package ru.ssau.tk._repfor2lab_._OOP_.io;
 
+import ru.ssau.tk._repfor2lab_._OOP_.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 final class FunctionsIO {
+
     private FunctionsIO(){
         throw new UnsupportedOperationException();
+    }
+
+    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+
+        PrintWriter boof = new PrintWriter(writer);
+
+        boof.println(function.getCount());
+
+        for (var point : function){
+            boof.printf("%f %f\n", point.x, point.y);
+        }
+
+        boof.flush();
     }
 }
