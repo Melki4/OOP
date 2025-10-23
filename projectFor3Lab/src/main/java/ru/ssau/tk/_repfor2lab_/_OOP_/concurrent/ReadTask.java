c5b1cad575ae040d65733ea163e11e7079e2790c
s4vl2 <lera0682@mail.ru>
2025-10-23 20:49:09 +0400

    добавлен класс ReadTask

---
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/concurrent/ReadTask.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk._repfor2lab_._OOP_.concurrent;
+
+import ru.ssau.tk._repfor2lab_._OOP_.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable{
+    private final TabulatedFunction function;
+    public ReadTask(TabulatedFunction func) {
+        this.function = func;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            double x = function.getX(i);
+            double y = function.getY(i);
+            System.out.printf("After read: i = %d, x = %f, y = %f", i, x, y);
+        }
+    }
+}
