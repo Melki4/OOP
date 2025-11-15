c385094b717cc206eeb29fc95a6396e86fbbeb0d
Nikolay PC <karikh.nickolay@icloud.com>
2025-11-16 02:16:37 +0400

    Just to create a comparable table

--- projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/databaseJDBC/createAComparebleTable.java
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/databaseJDBC/createAComparebleTable.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class createAComparebleTable {
+
     public void testWithATF(){
 
         pointsInterface pointsInterface = new pointsInterface();
--- projectFor3Lab/src/test/java/ru/ssau/tk/_repfor2lab_/_OOP_/databaseJDBC/createAComparebleTableTest.java
+++ projectFor3Lab/src/test/java/ru/ssau/tk/_repfor2lab_/_OOP_/databaseJDBC/createAComparebleTableTest.java
@@ -2,8 +2,6 @@ package ru.ssau.tk._repfor2lab_._OOP_.databaseJDBC;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class createAComparebleTableTest {
     @Test
     void test(){
