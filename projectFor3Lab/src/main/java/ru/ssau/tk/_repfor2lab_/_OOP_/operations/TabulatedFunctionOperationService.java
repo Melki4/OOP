d8d2967574ec8e4a903131b360e83b67f4e51ee0
Nikolay PC <karikh.nickolay@icloud.com>
2025-12-15 02:24:00 +0400

    Исправлена ошибка, из-за которой не происходило операции при изменении ф-ции

--- projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/operations/TabulatedFunctionOperationService.java
+++ projectFor3Lab/src/main/java/ru/ssau/tk/_repfor2lab_/_OOP_/operations/TabulatedFunctionOperationService.java
@@ -7,6 +7,8 @@ import ru.ssau.tk._repfor2lab_._OOP_.functions.factory.ArrayTabulatedFunctionFac
 import ru.ssau.tk._repfor2lab_._OOP_.functions.factory.TabulatedFunctionFactory;
 import ru.ssau.tk._repfor2lab_._OOP_.functions.*;
 
+import static java.lang.Math.abs;
+
 public class TabulatedFunctionOperationService {
 
     TabulatedFunctionFactory factory;
@@ -48,9 +50,6 @@ public class TabulatedFunctionOperationService {
         if (a.getCount()!=b.getCount()) {
             LOGGER.warn("Разное кол-во элементов в функциях");
             throw new InconsistentFunctionsException("Разное кол-во элементов в функциях");
-        } else if (a.leftBound()!= b.leftBound() || a.rightBound() != b.rightBound()){
-            LOGGER.warn("Разные элементы икс в массивах");
-            throw new InconsistentFunctionsException("Разные элементы икс в массивах");
         }
 
         Point[] aArray = asPoints(a);
@@ -60,6 +59,10 @@ public class TabulatedFunctionOperationService {
         double[] yValues = new double[aArray.length];
 
         for (int i=0; i< aArray.length; ++i){
+            if (abs(aArray[i].getX() - bArray[i].getX()) > 0.0001){
+                LOGGER.warn("Разные элементы икс в массивах ");
+                throw new InconsistentFunctionsException("Разные элементы икс в массивах");
+            }
             xValues[i] = aArray[i].x;
             yValues[i] = operation.apply(aArray[i].y, bArray[i].y);
         }
